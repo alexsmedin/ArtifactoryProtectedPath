dbce596ef10b98f86aae058d96e6d8e3dd600e4a
alexsmedin <74192239+alexsmedin@users.noreply.github.com>
2024-07-08 13:38:35 +0200

    Enable using regex pattern instead of hardcoded path

--- BlockOverwrite.groovy
+++ BlockOverwrite.groovy
@@ -1,12 +1,13 @@
 import org.artifactory.request.Request
 import org.artifactory.repo.RepoPath
+import java.util.regex.Pattern
 
-def protectedFilePath = "artifactory-repo/path/to/protected-file.txt"
+def protectedFilePath = ~/.*\/protected-file.txt/ // Change this to any regex pattern
 
 storage {
     beforeCreate { item ->
-        def repoPath = item.repoPath
-        if (repoPath.path == protectedFilePath && item.exists()) {
+        def repoPath = item.repoPath.path
+        if (Pattern.matches(protectedFilePath.pattern(), repoPath) && item.exists()) {
             cancel("Overwriting protected file is not allowed.", 403)
         }
     }
